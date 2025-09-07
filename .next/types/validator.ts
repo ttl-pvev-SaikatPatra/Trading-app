5fd468e4707eda17918b3c8cbbe3ebaeb0228da9
ttl-pvev-SaikatPatra <119476844+ttl-pvev-SaikatPatra@users.noreply.github.com>
2025-09-07 18:42:27 +0000

    feat: Add flowbite-react and react-icons dependencies

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -36,6 +36,12 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
 }
 
 
+// Validate ../../src/app/login/page.tsx
+{
+  const handler = {} as typeof import("../../src/app/login/page.js")
+  handler satisfies AppPageConfig<"/login">
+}
+
 // Validate ../../src/app/page.tsx
 {
   const handler = {} as typeof import("../../src/app/page.js")
