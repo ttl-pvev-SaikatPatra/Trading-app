70ed49952944cee49357fe366a9d394cb452a417
ttl-pvev-SaikatPatra <119476844+ttl-pvev-SaikatPatra@users.noreply.github.com>
2025-09-07 18:56:22 +0000

    feat: added apos at line 81 in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
           </Card>
           <Card>
             <h5 className="text-2xl font-bold tracking-tight text-gray-900">Universe</h5>
-            <p className="font-normal text-gray-700">Shows today's selected universe.</p>
+            <p className="font-normal text-gray-700">Shows today&apos;s selected universe.</p>
           </Card>
         </div>
       </div>
